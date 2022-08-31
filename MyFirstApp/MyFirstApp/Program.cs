733a3063be05a18d98f0dfad2e4ea1cd44b88caa
Gympast <112376007+Gympast@users.noreply.github.com>
2022-08-31 14:47:50 +0200

    tst

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,2 +1,11 @@
-﻿
-Console.WriteLine("Hello! My name is Johannes.");
+﻿var name = "Johannes";
+var age = 36;
+var breakfast = true;
+
+if (breakfast) {
+    Console.WriteLine($"Hi {name}! I see you are {age} years old. \nGood thing you ate breakfast today");
+}
+else
+{
+    Console.WriteLine($"Hi {name}! I see you are {age} years old. \nWhy didn't you eat any breakfast? Deathwish?");
+}
